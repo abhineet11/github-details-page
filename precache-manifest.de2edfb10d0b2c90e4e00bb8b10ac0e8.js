5c07247abcd3220b6ea122b86343764aa3f28a3e
apple <apple@apples-MacBook-Pro.local>
2020-06-03 18:43:18 +0530

    Updates

---
+++ precache-manifest.de2edfb10d0b2c90e4e00bb8b10ac0e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a97ab277048e35db764fae95bb3394f",
+    "url": "/github-details-page/index.html"
+  },
+  {
+    "revision": "5be81af0925fdaac3f32",
+    "url": "/github-details-page/static/css/main.b5da6cc3.chunk.css"
+  },
+  {
+    "revision": "17bc523e9022ac0ceca4",
+    "url": "/github-details-page/static/js/2.b7587ea3.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/github-details-page/static/js/2.b7587ea3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5be81af0925fdaac3f32",
+    "url": "/github-details-page/static/js/main.ca991740.chunk.js"
+  },
+  {
+    "revision": "7f2ae94828251f5c2997",
+    "url": "/github-details-page/static/js/runtime-main.ba7c701b.js"
+  }
+]);
